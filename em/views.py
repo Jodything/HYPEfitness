24ab67ce8a9d5625a0f44f229a241d58bcc18f7c
Jodything <Jodything@gmail.com>
2014-10-23 11:00:33 -0400

    facebook photo pull running

--- em/views.py
+++ em/views.py
@@ -2,7 +2,7 @@ from facepy import GraphAPI
 
 from django.shortcuts import render
 
-graph = GraphAPI('CAACEdEose0cBALU6a5GxPFtRBsu3gk1LASHbS1dviL4a16RcfmVuTR1fVuf9eMrscODDSaZABTwZAF4ilIPGxFRc8RrwVZBX6TtZA1sdFmPBMRDFFONDnFNZAUZB1lZBwb8FRzLhlpBV0t8iYcx198RzcKTswZAjjErGBhZChQzw8sd4lv4MGGjz1cEjo2i2LcsPOUxpDwkHdEU0wdWpbHzZBo')
+graph = GraphAPI('')
 
 
 def home(request):
@@ -15,9 +15,21 @@ def home(request):
     pic_three_title = photo['data'][2]['from']['name']
     pic_four = photo['data'][3]['source']
     pic_four_title = photo['data'][3]['from']['name']
+    pic_five = photo['data'][4]['source']
+    pic_five_title = photo['data'][4]['from']['name']
+    pic_six = photo['data'][5]['source']
+    pic_six_title = photo['data'][5]['from']['name']
+    pic_seven = photo['data'][6]['source']
+    pic_seven_title = photo['data'][6]['from']['name']
+    pic_eight = photo['data'][7]['source']
+    pic_eight_title = photo['data'][7]['from']['name']
     context = {'pic_one': pic_one, 'pic_one_title': pic_one_title,
                'pic_two': pic_two, 'pic_two_title': pic_two_title,
                'pic_three': pic_three, 'pic_three_title': pic_three_title,
-               'pic_four': pic_four, 'pic_four_title': pic_four_title,}
+               'pic_four': pic_four, 'pic_four_title': pic_four_title,
+               'pic_five': pic_five, 'pic_five_title': pic_five_title,
+               'pic_six': pic_six, 'pic_six_title': pic_six_title,
+               'pic_seven': pic_seven, 'pic_seven_title': pic_seven_title,
+               'pic_eight': pic_eight, 'pic_eight_title': pic_eight_title, }
     templates = 'home.html'
     return render(request, templates, context)
